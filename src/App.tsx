09216aa92fa297c7cd5083da89839d515f023b38
matheusoliv23 <matheushenri1996@gmail.com>
2022-03-30 19:58:52 -0300

    Resolvendo erros

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import 'styles/global.scss'
 
 import { Home } from 'pages/Home'
-import { Header } from 'components/Header'
-import Footer from 'components/Footer'
 
 export const App = () => {
   return (
     <>
-      <Header />
       <Home />
-      <Footer />
     </>
   )
 }
